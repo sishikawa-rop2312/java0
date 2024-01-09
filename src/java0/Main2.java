e7c1c4c25c59a504f49851d193ca6d943e0e4a55
sishikawa-rop2312 <tukuyomi610315@gmail.com>
2024-01-09 16:40:38 +0900

    first commit

---
+++ src/java0/Main2.java
@@ -0,0 +1,14 @@
+package java0;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int age;
+		age = 30;
+		
+		
+		System.out.println(age);
+	}
+
+}
---
+++ src/java0/R1_2.java
@@ -0,0 +1,17 @@
+package java0;
+
+public class R1_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int width = 3;
+		int height = 5;
+		int rectangleArea = width * height;
+		System.out.println("縦幅:" + width + " 横幅:" + height + "の長方形の面積は、" + rectangleArea + "です。");
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
